3aaf52dbb5d9f16bd27cf30681d379ca374a0cc7
OviedoF <oviedofederico39@gmail.com>
2023-12-04 16:40:10 -0300

    actualizaciones

--- src/models/PricesRules.model.js
+++ src/models/PricesRules.model.js
@@ -2,6 +2,7 @@ const {Schema, model} = require('mongoose');
 
 const PricesRulesSchema = new Schema({
     priceWholesalerOutGranada: {type: Number, default: 0},
+    priceOutGranada: {type: Number, default: 0},
 });
 
 module.exports = model('PricesRules', PricesRulesSchema);
